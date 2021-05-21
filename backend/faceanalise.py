3420400661a5a164010aab7b59f9ddfacdc7ba00
Wilbur <cin_jwilbur@uolinc.com>
2021-05-21 06:51:02 -0300

    organize def

--- backend/faceanalise.py
+++ backend/faceanalise.py
@@ -2,7 +2,7 @@ import boto3
 import json
 
 
-boto3.setup_default_session(profile_name='terraform_from_ni_notebook')
+boto3.setup_default_session(profile_name='terraform_from_ni_notebook') # when send to AWS are necessary comment
 s3 = boto3.resource('s3')
 client = boto3.client('rekognition')
 
@@ -41,23 +41,37 @@ def compara_imagens(faceId_detectadas):
                 MaxFaces=10,
             )
         )
-    # print(json.dumps(resultado_comparacao, indent=4))
+    print(json.dumps(resultado_comparacao, indent=4))
     return resultado_comparacao
 
 
 def gera_dados_json(resultado_comparacao):
     dados_json = []
     for face_matches in resultado_comparacao:
-        if(len(face_matches.get('FaceMatches'))) >=1:
+        if(len(face_matches.get('FaceMatches'))) >= 1:
             perfil = dict(nome=face_matches['FaceMatches'][1]['Face']['ExternalImageId'],
                           faceMatch=round(face_matches['FaceMatches'][1]['Similarity'], 2))
             dados_json.append(perfil)
     return dados_json
 
 
-faces_detectadas = detecta_faces()
-faceId_detectadas = cria_lista_faceId_detectadas(faces_detectadas)
-resultado_comparacao = compara_imagens(faceId_detectadas)
-dados_json = gera_dados_json(resultado_comparacao)
+def publica_dados(dados_json):
+    arquivo = s3.Object('fa-site-lambda-model', 'dados.json')
+    arquivo.put(Body=json.dumps(dados_json))
 
-print(json.dumps(dados_json, indent=4))
+
+def exclui_imagem_colecao(faceId_detectadas):
+    client.delete_faces(
+        CollectionId='faces',
+        FaceIds=faceId_detectadas,
+    )
+
+
+def main(event, context):
+    faces_detectadas = detecta_faces()
+    faceId_detectadas = cria_lista_faceId_detectadas(faces_detectadas)
+    resultado_comparacao = compara_imagens(faceId_detectadas)
+    dados_json = gera_dados_json(resultado_comparacao)
+    publica_dados(dados_json)
+    exclui_imagem_colecao(faceId_detectadas)
+    print(json.dumps(dados_json, indent=4))
